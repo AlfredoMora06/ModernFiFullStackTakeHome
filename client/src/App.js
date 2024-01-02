e20bd3d4269d1f83c430fc5001c24c9260ee14e2
Alfredo Morales <alfredomorales@Alfredos-MacBook-Pro.local>
2024-01-02 17:40:24 -0500

    adding modified files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react"
+import { Routes, Route } from "react-router-dom"
 
-import "./App.css"
-import StockTradingPlatform from "./views/Authenticated/StockTradingPlatform";
+import "./App.css";
+import StockTradingPlatform from "./views/StockTradingPlatform"
+import TickerHistory from "./views/TickerHistory"
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/0/history/:tickerSymbol" element={<TickerHistory />} />
         <Route
           path="*"
           element={<StockTradingPlatform to="/0/home" replace />}
--- client/src/libs/format.ts
+++ client/src/libs/format.ts
@@ -1,11 +1,8 @@
 // NOTE: amount MUST BE A NUMBER -- strings may not be formatted correctly
-export const formatMoney = (amount: number, notRounded?: boolean): string => {
+export const formatMoney = (amount: number): string => {
   let options: any = {
     style: "currency",
     currency: "USD",
-  }
-  if (notRounded) {
-    options.maximumSignificantDigits = 5
   }
   const formatter = new Intl.NumberFormat("en-US", options)
 
@@ -24,4 +21,18 @@ export function formatDateTimeLongMonth(
     minute: "2-digit",
   }
   return date.toLocaleDateString("en-US", dateLongOptions)
+}
+
+export function formatDateTimeShort(
+  dateString: Date, 
+): string {
+  const date = new Date(dateString)
+  const dateShortOptions: Intl.DateTimeFormatOptions = {
+    month: "2-digit",
+    day: "2-digit",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  }
+  return date.toLocaleDateString("en-US", dateShortOptions)
 }
